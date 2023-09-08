0fa2772e858cc1f1ae80dc44273c093072bc9ff7
Dania <danialara108@gmail.com>
2023-09-07 19:04:19 -0600

    Parcial prueba01

---
+++ app/src/main/java/com/example/parce1lara/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.parce1lara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
